7dc87def85b339118a0aa33df093c6a2debe7894
kate.levchenia <kale@brighta.biz>
2018-12-29 15:43:56 +0200

    added typed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-console.log('Hi');
+import Typed from 'typed.js';
+
+var options = {
+    strings: ["Привет", "Я очень сильно тебя люблю!", "Скоро приеду и обниму тебя!"],
+    typeSpeed: 40
+};
+
+var typed = new Typed("#typed", options);
