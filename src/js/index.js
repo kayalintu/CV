3d939fc2eaea09ce56c012e0dc42d0ee3cc1687a
Catherine <kaya.lintu@gmail.com>
2019-01-08 06:33:32 +0200

    fdsfd

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import jQuery from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
-
-jQuery(function() {
-    jQuery('body').css('color', 'blue');
-});
-=======
 // import Typed from 'typed.js';
 //
 // var options = {
@@ -21,21 +12,20 @@ jQuery(function() {
  */
 
 // get all elements with data-id
-let links = document.querySelectorAll('[data-id]');
+let links = document.querySelectorAll('.links');
 console.log(links);
 
 for(var i = 0; i < links.length; ++i) {
     var item = links[i];
     console.log(item);
-
 }
 
-// Array.prototype.forEach.call(links, function(el, i){
-//     let $id = '#' + this.getAttribute('data-id');
-//     console.log($id);
-// });
-//
 
+// var a = document.querySelectorAll('.links');
 
+//   for (var i = 0; i < a.length; i++) {
+//     if (a[i].matches('a[href$="#"]')) {
+//       console.log(a[i].href);
+//     }
+//   }
 
->>>>>>> e8498ebf54a08a25f4e6f85d760878a29842b570
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,4 @@
 const path = require('path');
-<<<<<<< HEAD
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const fs = require('fs')
-
-function generateHtmlPlugins(templateDir) {
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles.map(item => {
-    const parts = item.split('.');
-    const name = parts[0];
-    const extension = parts[1];
-    return new HtmlWebpackPlugin({
-      filename: `${name}.html`,
-      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-      inject: false,
-    })
-  })
-=======
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -35,105 +16,11 @@ function generateHtmlPlugins(templateDir) {
         inject: false,
     })
 })
->>>>>>> e8498ebf54a08a25f4e6f85d760878a29842b570
 }
 
 const htmlPlugins = generateHtmlPlugins('./src/html/views');
 
 module.exports = {
-<<<<<<< HEAD
-  entry: [
-    './src/js/index.js',
-    './src/scss/style.scss'
-  ],
-  output: {
-    filename: './js/bundle.js'
-  },
-  devtool: "source-map",
-  module: {
-    rules: [{
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                modules: false
-              }],
-            ],
-            plugins: ['@babel/plugin-proposal-class-properties'],
-          }
-        }
-      },
-      {
-        test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, 'src/scss'),
-        use: [{
-            loader: MiniCssExtractPlugin.loader,
-            options: {}
-          },
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              url: false
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              sourceMap: true,
-              plugins: () => [
-                require('cssnano')({
-                  preset: ['default', {
-                    discardComments: {
-                      removeAll: true,
-                    },
-                  }]
-                })
-              ]
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        include: path.resolve(__dirname, 'src/html/includes'),
-        use: ['raw-loader']
-      },
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "./css/style.bundle.css"
-    }),
-    new CopyWebpackPlugin([{
-        from: './src/fonts',
-        to: './fonts'
-      },
-      {
-        from: './src/favicon',
-        to: './favicon'
-      },
-      {
-        from: './src/img',
-        to: './img'
-      },
-      {
-        from: './src/uploads',
-        to: './uploads'
-      }
-    ]),
-  ].concat(htmlPlugins)
-=======
     entry: [
         './src/js/index.js',
         './src/scss/style.scss'
@@ -204,5 +91,4 @@ module.exports = {
             }
         ]),
     ].concat(htmlPlugins)
->>>>>>> e8498ebf54a08a25f4e6f85d760878a29842b570
 };
